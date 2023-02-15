ec6ef72a0b6525b4a1c5faf926e692049c29b164
Gibon300 <111329308+Gibon300@users.noreply.github.com>
2023-02-15 22:47:24 +0300

    Update range.js

--- scrypt/range.js
+++ scrypt/range.js
@@ -4,10 +4,38 @@ const inputSlider = document.querySelector(".land-area");
 inputSlider.oninput = () => {
   let value = inputSlider.value;
   sliderValue.textContent = value;
-  sliderValue.style.left = value / 2 + "%";
+
   sliderValue.classList.add("show");
 };
 
 inputSlider.onblur = () => {
   sliderValue.classList.remove("show");
+};
+
+const sliderValue2 = document.querySelector(".range-span-2");
+const inputSlider2 = document.querySelector(".house-area");
+
+inputSlider2.oninput = () => {
+  let value2 = inputSlider2.value;
+  sliderValue2.textContent = value2;
+
+  sliderValue2.classList.add("show");
+};
+
+inputSlider2.onblur = () => {
+  sliderValue2.classList.remove("show");
+};
+
+const sliderValue3 = document.querySelector(".range-span-3");
+const inputSlider3 = document.querySelector(".pool-area");
+
+inputSlider3.oninput = () => {
+  let value3 = inputSlider3.value;
+  sliderValue3.textContent = value3;
+
+  sliderValue3.classList.add("show");
+};
+
+inputSlider3.onblur = () => {
+  sliderValue3.classList.remove("show");
 };
